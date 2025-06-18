fae2941f6ef21f0f8fa67462891a534f443453b2
lwam <lwambsrat@gmail.com>
2025-06-18 12:12:11 +0300

    doc:Add testimonials to the informational website

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const cards = document.querySelectorAll('.testimonial-card');
+const dots = document.querySelectorAll('.dot');
+
+function setActiveCard(index) {
+    cards.forEach((card, i) => {
+        card.classList.toggle('active', i === index);
+    });
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === index);
+    });
+}
+
+dots.forEach((dot, idx) => {
+    dot.addEventListener('click', () => setActiveCard(idx));
+});
+
+cards.forEach((card, idx) => {
+    card.addEventListener('click', () => setActiveCard(idx));
+});
+
+    
+    
